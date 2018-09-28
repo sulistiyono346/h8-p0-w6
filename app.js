30c4548f0c249d17508e49be05560e2f8081a0f2
Sulistiyono <sulistiyono@Sulistio-MacBook-Pro.local>
2018-09-28 17:47:18 +0700

    versi 1.1 WeddingKU

--- app.js
+++ app.js
@@ -1,76 +1,57 @@
-const list = document.querySelector('#wedding-list ul');
-const forms = document.forms;
 
-// delete weddings
-list.addEventListener('click', (e) => {
-  if(e.target.className == 'delete'){
-    const li = e.target.parentElement;
+var list = document.querySelector('#wedding-list ul');
+var forms = document.forms;
+
+/*delete weddings with Arrow 
+list.addEventListener('click', (delete) => {
+    if(e.target.className === 'delete') {
+    var li = e.target.parentElement;
     li.parentNode.removeChild(li);
-  }
-});
+}
+// });*/
 
-// add weddings
-const addForm = forms['add-wedding'];
-addForm.addEventListener('submit', function(e){
-  e.preventDefault();
-
-  // create elements
-  const value = addForm.querySelector('input[type="text"]').value;
-  const li = document.createElement('li');
-  const weddingName = document.createElement('span');
-  const deleteBtn = document.createElement('span');
-
-  // add text content
-  weddingName.textContent = value;
-  deleteBtn.textContent = 'delete';
-
-  // add classes
-  weddingName.classList.add('name');
-  deleteBtn.classList.add('delete');
-
-  // append to DOM
-  li.appendChild(weddingName);
-  li.appendChild(deleteBtn);
-  list.appendChild(li);
-});
-
-// hide weddings
-const hideBox = document.querySelector('#hide');
-hideBox.addEventListener('change', function(e){
-  if(hideBox.checked){
-    list.style.display = "none";
-  } else {
-    list.style.display = "initial";
-  }
-});
-
-// filter weddings
-const searchBar = forms['search-weddings'].querySelector('input');
-searchBar.addEventListener('keyup', (e) => {
-  const term = e.target.value.toLowerCase();
-  const weddings = list.getElementsByTagName('li');
-  Array.from(weddings).forEach((wedding) => {
-    const title = wedding.firstElementChild.textContent;
-    if(title.toLowerCase().indexOf(e.target.value) != -1){
-      wedding.style.display = 'block';
-    } else {
-      wedding.style.display = 'none';
+list.addEventListener('click', function (del) {
+    if (del.target.className === 'delete') {
+        var li = del.target.parentElement;
+        li.parentNode.removeChild(li);
     }
-  });
 });
 
-// tabbed content
-const tabs = document.querySelector('.tabs');
-const panels = document.querySelectorAll('.panel');
-tabs.addEventListener('click', (e) => {
-  if(e.target.tagName == 'LI'){
-    const targetPanel = document.querySelector(e.target.dataset.target);
-    Array.from(panels).forEach((panel) => {
-      if(panel == targetPanel){
-        panel.classList.add('active');
-      }else{
-        panel.classList.remove('active');
-      }
+
+
+var addForm = forms['add-wedding'];
+addForm.addEventListener('submit', function (add) {
+    add.preventDefault();
+
+    // create elements
+    var value = addForm.querySelector('input[type="text"]').value;
+    var li = document.createElement('li');
+    var weddingName = document.createElement('span');
+    var deleteBtn = document.createElement('span');
+
+    // add text content
+    weddingName.textContent = value;
+    deleteBtn.textContent = 'delete';
+
+    // add classes
+    weddingName.classList.add('name');
+    deleteBtn.classList.add('delete');
+
+    // append to DOM
+    li.appendChild(weddingName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+});
+
+var searchBar = forms['search-data'].querySelector('input');
+searchBar.addEventListener('keyup', function (find) {
+    var weddings = list.getElementsByTagName('li');
+    Array.from(weddings).forEach(function (wedding) {
+        var title = wedding.firstElementChild.textContent;
+        if (title.toLowerCase().indexOf(find.target.value) != -1) {
+            wedding.style.display = 'block';
+        } else {
+            wedding.style.display = 'none';
+        }
     });
-  }
 });
